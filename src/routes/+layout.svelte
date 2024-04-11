81896c70993e1610b5795cffb4406b92b8c73cac
Daniel <danielimmanj@gmail.com>
2024-04-11 21:42:31 +0530

    layout changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,21 +13,21 @@
 </script>
 
 <div
-  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
-></div>
-<div
-  class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10]" +
-    (y > 0 ? " opacity-full pointer-events-auto" : " pointer-events-none")}
+    class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-  <button
-    on:click={goTop}
-    class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
-  >
-    <i class="fa-solid fa-arrow-up" />
-  </button>
+    <div
+        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+            (y > 0 ? " opacity-full pointer-events-auto" : " pointer-events-none opacity-0")}
+    >
+        <button
+            on:click={goTop}
+            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+        >
+            <i class="fa-solid fa-arrow-up" />
+        </button>
+    </div>
+    <Header {y} {innerHeight}/>
+    <slot />
+    <Footer />
 </div>
-<Header {y} />
-<slot />
-<Footer></Footer>
-
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
