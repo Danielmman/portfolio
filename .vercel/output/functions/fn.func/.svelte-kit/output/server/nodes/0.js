b5b9c2d9e629ac540f55d70765040b460f250bb3
Daniel <danielimmanj@gmail.com>
2024-04-11 23:34:36 +0530

    Revert "deployed application in vercel"
    This reverts commit 1fe4d9307d26f2cb062c5fd6c65673c1d1548eae.

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.CNu0IUCq.js","_app/immutable/chunks/scheduler.B2F6oQEs.js","_app/immutable/chunks/index.BCD-owaQ.js","_app/immutable/chunks/each.D6YF6ztN.js"];
-export const stylesheets = ["_app/immutable/assets/0.B6pRZ5Bb.css"];
-export const fonts = [];
